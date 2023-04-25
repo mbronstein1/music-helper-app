9df5639931fd4007a16967c888ab5130f9d1ada0
Matthew Bronstein <mbronstein1@gmail.com>
2023-04-25 12:08:43 -0500

    added auth middleware to protect routes to get users info

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -1,9 +1,10 @@
 // route: '/api/user'
 const router = require('express').Router();
+const { authMiddleware } = require('../../utils/auth');
 const { loginUser, createUser, getAllUsers, getUserById } = require('../../controllers/userControllers');
 
-router.get('/:userRole', getAllUsers);
-router.get('/:userRole/:id', getUserById);
+router.get('/:userRole', authMiddleware, getAllUsers);
+router.get('/:userRole/:id', authMiddleware, getUserById);
 router.post('/login/:userRole', loginUser);
 router.post('/signup/:userRole', createUser);
 
