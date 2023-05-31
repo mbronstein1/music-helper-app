7a0590a2d5b7a9122dac5a1562045e785cc74e66
Matthew Bronstein <mbronstein1@gmail.com>
2023-05-31 16:52:34 -0500

    setup auth service on client

---
+++ client/src/utils/auth.js
@@ -0,0 +1,43 @@
+import { redirect } from 'react-router-dom';
+
+class AuthService {
+  getTokenDuration() {
+    const storedExpirationDate = localStorage.getItem('expiration');
+    const expirationDate = new Date(storedExpirationDate);
+    const now = new Date();
+    const duration = expirationDate.getTime() - now.getTime();
+    return duration;
+  }
+
+  login(user) {
+    localStorage.setItem('user', JSON.stringify(user));
+    const expiration = new Date();
+    expiration.setHours(expiration.getHours() + 2);
+    localStorage.setItem('expiration', expiration.toISOString());
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+    localStorage.removeItem('expiration');
+    return redirect('/');
+  }
+
+  getUser() {
+    const user = JSON.parse(localStorage.getItem('user'));
+    const tokenDuration = this.getTokenDuration();
+
+    if (!user) {
+      return null;
+    }
+
+    if (tokenDuration <= 0) {
+      return this.logout();
+    }
+
+    return user;
+  }
+}
+
+const Auth = new AuthService();
+
+export default Auth;
